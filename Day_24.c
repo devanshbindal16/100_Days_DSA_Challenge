e4b49810d1c6ffd7848cf79f6bc3b53d53313ac3
Devansh Bindal <devanshbindal16@gmail.com>
2026-02-24 22:28:51 +0530

    DAY_24.c

---
+++ Day_24.c
@@ -0,0 +1,99 @@
+// Problem: Delete First Occurrence of a Key - Implement using linked list with dynamic memory allocation.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+// - Third line: integer key
+
+// Output:
+// - Print the linked list elements after deletion, space-separated
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+// 30
+
+// Output:
+// 10 20 40 50
+
+// Explanation:
+// Traverse list, find first node with key, remove it by adjusting previous node's next pointer.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Delete first occurrence of key
+struct Node* deleteFirstOccurrence(struct Node* head, int key) {
+    struct Node *temp = head, *prev = NULL;
+
+    // If head contains key
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    // Search for key
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key not found
+    if (temp == NULL)
+        return head;
+
+    // Remove node
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+int main() {
+    int n, value, key;
+    struct Node *head = NULL, *temp = NULL;
+
+    scanf("%d", &n);
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    scanf("%d", &key);
+
+    head = deleteFirstOccurrence(head, key);
+
+    // Print updated list
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
