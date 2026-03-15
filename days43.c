631743841eb8886e42b4b18fc9200d6ebb08f2be
Devansh Bindal <devanshbindal16@gmail.com>
2026-03-15 10:20:42 +0530

    DAY_43

---
+++ days43.c
@@ -0,0 +1,76 @@
+/*
+Problem Statement:
+Construct a Binary Tree from the given level-order traversal.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers (-1 represents NULL)
+
+Output Format:
+- Print inorder traversal of the constructed tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 6
+
+Output:
+4 2 5 1 3 6
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to build tree from level order array
+struct Node* buildTree(int arr[], int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[i]);
+
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+
+    return root;
+}
+
+// Inorder traversal
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n, 0);
+
+    inorder(root);
+
+    return 0;
+}
