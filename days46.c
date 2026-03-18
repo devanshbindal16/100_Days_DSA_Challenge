77ecbe2c977fb530abe39482fb6d084e0c5dfc12
Devansh Bindal <devanshbindal16@gmail.com>
2026-03-18 09:46:46 +0530

    DAY_46

---
+++ days46.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of Tree Node
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create new node
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order input
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode* root = createNode(arr[0]);
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(sizeof(struct TreeNode*) * n);
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* current = queue[front++];
+
+        // Left child
+        if (i < n && arr[i] != -1) {
+            current->left = createNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            current->right = createNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Level Order Traversal
+void levelOrder(struct TreeNode* root) {
+    if (root == NULL) return;
+
+    struct TreeNode** queue = (struct TreeNode**)malloc(sizeof(struct TreeNode*) * 10000);
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            struct TreeNode* node = queue[front++];
+            printf("%d ", node->val);
+
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+        printf("\n"); // new level
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    levelOrder(root);
+
+    return 0;
+}
