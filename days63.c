622c8c8bd20055556be0b73e8373059622fc6c52
Devansh Bindal <devanshbindal16@gmail.com>
2026-04-04 22:54:33 +0530

    DAY_63

---
+++ days63.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];   // adjacency matrix
+int visited[MAX];    // visited array
+int n;               // number of vertices
+
+// DFS function
+void dfs(int v) {
+    printf("%d ", v);
+    visited[v] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int s;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &s);
+
+    printf("DFS Traversal: ");
+    dfs(s);
+
+    return 0;
+}
